4e2818462b4bc32a172b208fc7e15194b3f7046d
Isaiah Nields <imnields@gmail.com>
2019-02-26 10:35:54 -0500

    Update Server

--- app/server.py
+++ app/server.py
@@ -8,11 +8,13 @@ from app.graphs.line_plot import graph as line_graph
 from app.graphs.histograms import fig as histograms
 from tripadvisor.topo import fig as topograph
 from tripadvisor.dense_topology_predicted import fig as dense_topo
+import base64
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 graphs = [dcc.Graph(figure=map, style={'height': '800px', 'width': '50%', 'float': 'left'}, config={'staticPlot': True}) for map in maps]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 
 app.layout = html.Div(children=[
     html.H1(children='An in-depth graphical exploration of NYC hotels, January 2019'),
@@ -20,12 +22,11 @@ app.layout = html.Div(children=[
     html.Div(style={'height': '1600px', 'width': '100%'}),
     dcc.Graph(figure=histograms, style={'height': '1000px'}),
     dcc.Graph(figure=heatmap, style={'height': '1000px'}),
-    dcc.Graph(figure=recommendation, style={'height': '700px'}),
     dcc.Graph(figure=line_graph, style={'height': '1000px'}),
+    dcc.Graph(figure=recommendation, style={'height': '700px'}),
     dcc.Graph(figure=topograph, style={'height': '1000px'}),
     dcc.Graph(figure=dense_topo, style={'height': '1000px'})
 ])
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
